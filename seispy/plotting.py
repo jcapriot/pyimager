4d636058bd52cd730959123b0e11106a352256fa
Joseph Capriotti <josephrcapriotti@gmail.com>
2025-02-28 13:02:13 -0700

    rudimentary plotting improvements

--- seispy/plotting.py
+++ seispy/plotting.py
@@ -1,3 +1,4 @@
+import warnings
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -6,13 +7,16 @@ def wiggle(data, ax=None, color='k'):
     if ax is None:
         plt.figure()
         ax = plt.gca()
-        ax.invert_yaxis()
 
     trace_plots = []
     line_plots = []
+    y_max = 0
     for i, trace in enumerate(data):
         n1 = trace.ns
         dt = trace.dt / 1_000_000
+        ymax = max(y_max, dt * (n1 - 1))
+        if dt == 0:
+            warnings.warn("trace has a 0 sample spacing.")
         s_locs = np.linspace(0, n1*dt, n1, endpoint=False)
         dat = np.asarray(trace)
 
@@ -20,6 +24,8 @@ def wiggle(data, ax=None, color='k'):
         line = ax.plot(dat+i, s_locs, color=color)
         trace_plots.append(fill)
         line_plots.append(line)
+    ax.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
+    ax.set_ylim([ymax, 0])
     return trace_plots, line_plots
 
 def image(data, ax=None, cmap='seismic', **kwargs):
@@ -36,5 +42,7 @@ def image(data, ax=None, cmap='seismic', **kwargs):
             kwargs['vmin'] = -max_v
         if 'vmax' not in kwargs:
             kwargs['vmax'] = max_v
+
+    ax.tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
 
     return ax.pcolormesh(im_dat, cmap=cmap, **kwargs)
